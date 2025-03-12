4ac9d54160002df3a0397c2cfc24640521a297a3
Aravind MV <aravind.mv@quantumacumen.com>
2025-03-12 10:31:08 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,14 @@
 curl -L -o docker-image.tar "https://github.com/mr-mister007/react/releases/download/latest/docker-image.tar"
 #load
 # Load the Docker image
-docker load -i docker-122image.tar
+docker load -i docker-1224image.tar
 #load images
 # Stop and remove the existing container if it exists
 
 docker stop my-react-container || true or false
-docker \ your-react-container || false
+docker rm-rf  your-react-container || false
 
 # Run the new container
 docker run -d --name my-new-react-container -p 80:80 this-react-app
+
+#added images 
